4dee080054071006791de8b4a334a2adc76f7116
yjtan0819 <tan.yijia0819@gmail.com>
2023-01-10 21:54:20 -0500

    Update files

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ function makeSound(key) {
 }
 
 function buttonAnimation(currentKey) {
-    var activeButton = document.querySelector("." + currentKey);
-    activeButton.classList.add("pressed");
-    setTimeout(function() {
-        activeButton.classList.remove("pressed");
-    }, 100);
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
